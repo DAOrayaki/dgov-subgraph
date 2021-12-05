347a593e1e5fa172c493256f5a1bf260582f4da5
lxl <dreammakerlxl@gmail.com>
2021-12-05 13:33:03 +0800

    merge graph

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const config = require('./config.json')
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const MNEMONIC = 'my mnemonic words from metamask'
 
 module.exports = {
   compilers: {
@@ -17,7 +19,20 @@ module.exports = {
       network_id: '*',
       gas: 6721970,
       gasPrice: 22000000000 // Specified in Wei
+    },
+    local: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/my-key")
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
     }
+
     // development: {
     //   host: '127.0.0.1',
     //   port: 8545,
